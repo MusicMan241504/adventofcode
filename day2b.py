1fae33dc2b1419b916f1e766829edb918870cfbd
Isaac Lavender <musicman241504@gmail.com>
2022-12-03 14:11:41 +0000

    day 2

---
+++ day2b.py
@@ -0,0 +1,26 @@
+file = open("day2.txt")
+score = 0
+for line in file:
+    if line.find('A X') != -1:#z
+        score = score + 3+0
+    elif line.find('B X') != -1:#x
+        score = score + 1+0
+    elif line.find('C X') != -1:#y
+        score = score + 2+0
+
+    elif line.find('A Y') != -1:#x
+        score = score + 1+3
+    elif line.find('B Y') != -1:#y
+        score = score + 2+3
+    elif line.find('C Y') != -1:#z
+        score = score + 3+3
+
+    elif line.find('A Z') != -1:#y
+        score = score + 2+6
+    elif line.find('B Z') != -1:#z
+        score = score + 3+6
+    elif line.find('C Z') != -1:#x
+        score = score + 1+6
+
+print(score)
+
