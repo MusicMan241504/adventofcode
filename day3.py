9687bfafd16be5422a53fcc7fd612fd6d1250528
Isaac Lavender <musicman241504@gmail.com>
2022-12-03 14:41:39 +0000

    day3

---
+++ day3.py
@@ -0,0 +1,14 @@
+import string
+letters = string.ascii_letters
+file = open('day3.txt')
+total = 0
+for line in file:
+    text = line.strip()
+    l = text[:int(len(text)/2)]
+    r = text[int(len(text)/2):]
+    c = ''
+    for char in l:
+        if char in r:
+            total = total + letters.find(char)+1
+            c = char
+print(total)
