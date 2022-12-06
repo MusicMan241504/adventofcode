bda939ed5ab1411c40e814ec056bb38ae3dc7c06
Isaac Lavender <musicman241504@gmail.com>
2022-12-06 20:19:35 +0000

    day6

---
+++ day6b.py
@@ -0,0 +1,14 @@
+file = open('day6.txt')
+text = file.readline()
+count = 1
+nums = []
+for i in range(14,len(text)):
+    tmp = text[i-14:i]
+    found = False
+    for char in tmp:
+        if tmp.find(char) != tmp.rfind(char):
+            found = True
+    if not found:
+        nums.append(i)
+print(nums[0])
+file.close()
