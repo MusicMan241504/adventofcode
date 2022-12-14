1be5d467665a99d2714f3a262487ce1d2c60e8dc
Isaac Lavender <musicman241504@gmail.com>
2022-12-14 11:22:58 +0000

    removed pointless line

--- day3.py
+++ day3.py
@@ -6,7 +6,6 @@ for line in file:
     text = line.strip()
     l = text[:int(len(text)/2)]
     r = text[int(len(text)/2):]
-    c = ''
     for char in l:
         if char in r:
             total = total + letters.find(char)+1
